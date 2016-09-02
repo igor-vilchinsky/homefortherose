b91d38533759597c8e894a525d3eca51d2a286c6
igrzxc <igrzxc@gmail.com>
2016-09-02 06:16:34 +0300

    first commit

---
+++ src/main/java/com/homefortherose/HomefortheroseApplication.java
@@ -0,0 +1,12 @@
+package com.homefortherose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomefortheroseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomefortheroseApplication.class, args);
+	}
+}
